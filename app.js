7a65cd5ce1f08805b9fa6a9cd41e4009561be805
JakobVapper <jakob.vapper@ametikool.ee>
2024-01-15 08:44:50 +0200

    Added the letter typing possibility

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
-const alphabet = 'ABCDEFGHIJKLMNOPQRSŠZŽTUVWÕÄÖÜXY'.split('');
-const word = 'AMETIKOOL'.split(' ');
+const alphabet = 'ABDEFGHIJKLMNOPRSŠZŽTUVÕÄÖÜ'.split('');
+const word = 'AMETIKOOL'.split('');
 let guess = [...Array(word.length).keys()].map(i => '_');
 
 const wordDiv = document.getElementById('word');
 
 for ( let i in guess ) {
     const letterDiv = document.createElement('div');
-    letterDiv.setAttribute('id', 'letter_');
+    letterDiv.setAttribute('id', 'letter_' + i);
     letterDiv.innerText = guess[i];
-    wordDiv.append(letterDiv);
-};
+    wordDiv.appendChild(letterDiv);
+}
 
 document.addEventListener('keyup', e => {
-    if (alphabet.includes(e.key.toUpperCase())) {
-        console.log(e.key);
+    const g = e.key.toUpperCase();
+    if ( alphabet.includes(g) ) {
+        if ( word.includes(g) ) {
+            for ( let i in word) {
+                if ( word[i] == g ) {
+                    const letterDiv = document.getElementById('letter_' + i);
+                    letterDiv.innerText = g;
+                }
+            }
+        }
     }
-}
-)
+});
