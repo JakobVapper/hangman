c6b2161235ac086fde9db1afd4028bd422ce80d0
JakobVapper <jakob.vapper@ametikool.ee>
2024-01-15 11:35:37 +0200

    Added the alphabet

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const alphabet = 'ABDEFGHIJKLMNOPRSŠZŽTUVÕÄÖÜ'.split('');
+const alphabet = 'ABCDEFGHIJKLMNOPQRSŠZŽTUVWÕÄÖÜXY'.split('');
 const word = 'AMETIKOOL'.split('');
+const alphabetDiv = document.getElementById('alphabet');
 let guess = [...Array(word.length).keys()].map(i => '_');
+let wrongGuesses = 0;
 
 const wordDiv = document.getElementById('word');
 
@@ -9,6 +11,13 @@ for ( let i in guess ) {
     letterDiv.setAttribute('id', 'letter_' + i);
     letterDiv.innerText = guess[i];
     wordDiv.appendChild(letterDiv);
+}
+
+for ( let i in alphabet ) {
+    const aDiv = document.createElement('Div');
+    aDiv.setAttribute('id', alphabet[i]);
+    aDiv.innerText = alphabet[i];
+    alphabetDiv.appendChild(aDiv);
 }
 
 document.addEventListener('keyup', e => {
@@ -21,6 +30,16 @@ document.addEventListener('keyup', e => {
                     letterDiv.innerText = g;
                 }
             }
+        } else {
+            if ( !wrongGuesses.includes(g) ) {
+                score--;
+
+                const scoreImg = document.querySelector('#score img');
+                scoreImg.setAttribute('src', 'img/' + score + '.png');
+            }
         }
+
+        const aDiv = document.createElementById(g);
+        aDiv.classlist.app('grayed-out')
     }
 });
